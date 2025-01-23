8167a1cd7b44f5c4834cc211486d0202f0cdebca
Abdulmohsen28 <a_abulhasan@biswashington.org>
2025-01-23 17:49:31 +0000

    hi

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-
+question = "Who is the ruler of Mars?"
+print(question) 
+user_answer = input("Your answer: ")
+if user_answer.lower() == "Pascal":
+    print("Correct")
+else:
+    ("OOOPS! The correct answer is Pascal")
+score = 0
+score += 1
+print(f"Your score is: ")
